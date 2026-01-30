fe71792dcabf35009dc5ada656fade56501cd40d
parultandon1999 <parul.tandon.1999@gmail.com>
2026-01-30 12:08:59 +0530

    final navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,14 @@
-import { Moon, Sun } from '@phosphor-icons/react'
+import { useState } from 'react'
+import { Moon, Sun, List, X } from '@phosphor-icons/react'
 import '../styles/Navbar.css'
 
 function Navbar({ darkMode, toggleDarkMode, currentPage }) {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen)
+  }
+
   return (
     <header className="header">
       <button 
@@ -11,8 +18,14 @@ function Navbar({ darkMode, toggleDarkMode, currentPage }) {
       >
         {darkMode ? <Sun size={20} /> : <Moon size={20} />}
       </button>
-
-      <nav className="nav">
+      <button 
+        className="mobile-menu-btn" 
+        onClick={toggleMenu}
+        aria-label="Toggle menu"
+      >
+        {isMenuOpen ? <X size={24} /> : <List size={24} />}
+      </button>
+      <nav className={`nav ${isMenuOpen ? 'open' : ''}`}>
         <a href="/" className={currentPage === 'home' ? 'active' : ''}>
           Home
         </a>
